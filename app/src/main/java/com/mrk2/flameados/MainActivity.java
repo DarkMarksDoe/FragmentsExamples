4d236cc5baf9c10372ae81f14515f8459f434751
darkmarksdoe <darkmarksdoe@gmail.com>
2019-10-25 03:28:01 -0500

    Created Fragment examples, send messages into diferent fragments

---
+++ app/src/main/java/com/mrk2/flameados/MainActivity.java
@@ -0,0 +1,86 @@
+package com.mrk2.flameados;
+
+import android.net.Uri;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+    public class MainActivity extends AppCompatActivity implements View.OnClickListener, FragmentDark.OnFragmentInteractionListener, FragmentLight.OnFragmentInteractionListener {
+
+        boolean light;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //The default value on light is on, so, LightFragment is default
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.principalFrame,new FragmentLight())
+                .commit();
+        light=true;
+
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                //                        .setAction("Action", null).show();
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                if(light){
+                    fragmentTransaction.replace(R.id.principalFrame,new FragmentDark());
+                }else{
+                    fragmentTransaction.replace(R.id.principalFrame,new FragmentLight());
+                }
+                fragmentTransaction.commit();
+                light=!light;
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) { //method created by implements
+
+    }
+
+        @Override
+        public void onFragmentInteraction(String text) {
+
+        }
+    }
